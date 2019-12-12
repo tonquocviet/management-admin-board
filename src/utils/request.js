4a8132a1b164b1370742808aad6d81ba15083e8c
htactive-viettq <viettq@htactive.com>
2019-12-12 11:39:31 +0700

    handle 500

--- src/utils/request.js
+++ src/utils/request.js
@@ -19,19 +19,22 @@ const handle500 = () => {
 
 const errorHandler = error => {
   const { response } = error;
+  const urlName = window.location.pathname;
 
   if (response && response.status) {
-    if (response.status === 401) {
-      return handle401(error);
-    }
-    if (response.status === 403) {
-      return handle403();
-    }
-    if (response.status === 500) {
-      return handle500();
+    if (urlName !== '/user/login') {
+      if (response.status === 401) {
+        return handle401(error);
+      }
+      if (response.status === 403) {
+        return handle403();
+      }
+      if (response.status === 500) {
+        return handle500();
+      }
     }
     response.json().then(res => {
-      errorMessageHandler(response.status, res.message);
+      errorMessageHandler(response.status, res.message || 'Xảy ra lỗi không xác định được');
     });
   }
   return null;
